caf483b012c8519d724ccf762a3ddc8d184a62b3
Arpit <arpitdahat.ad@gmail.com>
2023-01-29 13:56:26 +0530

    WhatsApp

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -20,18 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class WhatsappController {
 
     //Autowire will not work in this case, no need to change this and add autowire
+    @Autowired
     WhatsappService whatsappService = new WhatsappService();
 
     @PostMapping("/add-user")
     public String createUser(String name, String mobile) throws Exception {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
-
         return whatsappService.createUser(name, mobile);
     }
 
     @PostMapping("/add-group")
-    public Group createGroup(List<User> users){
+    public Group createGroup(List<User> users) throws Exception {
         // The list contains at least 2 users where the first user is the admin. A group has exactly one admin.
         // If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
         // If there are 2+ users, the name of group should be "Group count". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
@@ -41,6 +41,7 @@ public class WhatsappController {
         //For example: Consider userList1 = {Alex, Bob, Charlie}, userList2 = {Dan, Evan}, userList3 = {Felix, Graham, Hugh}.
         //If createGroup is called for these userLists in the same order, their group names would be "Group 1", "Evan", and "Group 2" respectively.
 
+
         return whatsappService.createGroup(users);
     }
 
@@ -49,7 +50,13 @@ public class WhatsappController {
         // The 'i^th' created message has message id 'i'.
         // Return the message id.
 
-        return whatsappService.createMessage(content);
+
+        try {
+            return whatsappService.createMessage(content);
+        }
+        catch(Exception e){
+            return 0;
+        }
     }
 
     @PutMapping("/send-message")
@@ -66,6 +73,7 @@ public class WhatsappController {
         //Throw "Approver does not have rights" if the approver is not the current admin of the group
         //Throw "User is not a participant" if the user is not a part of the group
         //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
+
 
         return whatsappService.changeAdmin(approver, user, group);
     }
